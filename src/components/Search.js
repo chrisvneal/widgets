7d4a379680aa3e5ff83e6f20c94b2fa9079383fe
Chris Neal <chrisvneal@gmail.com>
2022-04-06 22:57:57 -1000

    Create text input for Search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,12 @@ import React from "react";
 const Search = () => {
   return (
     <div>
-      <h1>Search</h1>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter search term</label>
+          <input className="input" />
+        </div>
+      </div>
     </div>
   );
 };
