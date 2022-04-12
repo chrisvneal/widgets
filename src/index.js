bfc0ba0d13d0207c17aa025838d544deb49ba516
Chris Neal <chrisvneal@gmail.com>
2022-04-12 05:16:47 -1000

    Use React's NEW root API instead of the Legacy root API

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOMClient from "react-dom/client";
 import App from "./App";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+const container = document.getElementById("root");
+const root = ReactDOMClient.createRoot(container);
+
+root.render(<App />);
+
+// Legacy React root API
+// ReactDOM.render(<App />, document.querySelector("#root"));
