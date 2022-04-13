d17d1f72ae42e1266c246d04aaef93a916ad06e7
Chris Neal <chrisvneal@gmail.com>
2022-04-13 00:57:13 -1000

    Add initial async request in useEffect

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,7 +4,13 @@ import axios from "axios";
 const Search = () => {
   const [term, setTerm] = useState("");
 
-  useEffect(() => {}, [term]);
+  useEffect(() => {
+    const search = async () => {
+      await axios.get("");
+    };
+
+    search();
+  }, [term]);
   return (
     <div>
       <div className="ui form">
