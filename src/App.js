99c5db3694e561b7f464bc1c28049fb3fc3e9024
Chris Neal <chrisvneal@gmail.com>
2022-04-21 13:29:39 -1000

    Show Translate component in App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
+import Translate from "./components/Translate";
 
 const items = [
   {
@@ -49,6 +50,7 @@ export default () => {
           onSelectedChange={setSelected}
         />
       ) : null}
+      <Translate />
     </div>
   );
 };
