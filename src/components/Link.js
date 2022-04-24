bdd0280ab6c27f2549a8d7f4f708ad722cec287b
Chris Neal <chrisvneal@gmail.com>
2022-04-23 15:25:18 -1000

    Attach onClick event handler to Link component's 'a' element

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-const Link = () => {
-  return <a>Link</a>;
+const Link = ({ className, href, children }) => {
+  const onClick = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <a onClick={onClick} className={className} href={href}>
+      {children}
+    </a>
+  );
 };
 
 export default Link;
